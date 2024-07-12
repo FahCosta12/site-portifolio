afe0355ce737ea8b2b034600a50782125a374e43
Fabrício Costa <fabricio_matheus@outlook.com>
2024-07-12 00:41:25 -0300

    primeiro commit. estrutura e estilos NavHorizontal

---
+++ src/components/NavHorizontal/navHorizontal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './styles.css';
+
+
+
+const NavHorizontal = () => {
+    return (
+        <nav>
+            <ul>
+            <header>Fabrício Costa</header>
+                <li>
+                <button>Home</button>
+                </li>
+
+                <li>
+                    <button>Sobre</button>
+                </li>
+
+                <li>
+                    <button >Portifólio</button>
+                </li>
+
+                <li>
+                    <button>Contato</button>
+                </li>
+            </ul>
+            
+        </nav>
+    )
+}
+
+export default NavHorizontal;
