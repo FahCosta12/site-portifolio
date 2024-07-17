cf8c84bacb911275cb47ec9ece4d686f89c574ce
Fabrício Costa <fabricio_matheus@outlook.com>
2024-07-17 00:08:32 -0300

    inicio do footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,17 @@
+import Stack from 'react-bootstrap/Stack';
 import './footer.css';
 
 const Footer = () => {
     return (
-        <div className='footer'>
+        <Stack direction="horizontal" gap={3} className='stack'>
             footer
-        </div>
-
-    )
+            <div className='p-2'>HTML5</div>
+            <div className='p-2'>CSS3</div>
+            <div className='p-2'>Javascript</div>
+            <div className='p-2'>ReactJs</div>
+            <div className='p-2'>NodeJs</div>
+        </Stack>
+    );
 }
 
 export default Footer;
