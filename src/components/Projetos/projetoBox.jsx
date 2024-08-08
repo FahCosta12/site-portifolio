60b66f0376072a619529a76b3d388de3b20b5eec
Fabrício Costa <fabricio_matheus@outlook.com>
2024-08-07 23:29:15 -0300

    alteracoes no NAV e PROJETOBOX

--- src/components/Projetos/projetoBox.jsx
+++ src/components/Projetos/projetoBox.jsx
@@ -8,14 +8,17 @@ const ProjetoBox = ({projetoFoto, projetoNome}) => {
         MarketListDesc:
         "Projeto simples feito para mobile utilizando o Expo + Javascript com os frameworks Reactjs e React-Native e estilizado com Styled-Component, imaginando uma lista de supermercado. Adicionando itens existentes em um supermercado à lista. Projeto feito para treinamento pessoal apenas para visualizar minhas habilidade no Front - End.",
         MarketListGithub: "https://github.com/FahCosta12/market-list-app.git",
+        MarketListWebSite: "",
 
         CounterDesc:
         "Componente bem amador kkk. Feito para mobile utilizando o Expo + Javascript com os frameworks Reactjs e React-Native e estilizado com Styled-Component, imaginando um contator que ao clicar no botão (+) ou (-) incrementa ou decrementa em ordem sequêncial os números decimais. Projeto feito para treinamento pessoal apenas para visualiazar minhas habilidades no Front - End.",
         CounterGithub: "https://github.com/FahCosta12/Counter.git",
+        CounterWebSite: "",
 
         DoceBrigadeiroDesc:
         "Projeto feito com a ideia de desenvolver um app mobile para uma suposta loja de doces. Que contem as telas para o usuario realizar seu cadastro (SignUp), faça o seu login caso ja tenha uma conta cadastrada (SignIn) e uma Esqueci minha senha (Forgot Password) que ao clicar no botão Forgot Password, encaminha para a tela aonde o usuario inseri seu e-mail para criar uma nova senha. Foi feito utilizando Expo + Typescript e Javascript com os frameworks Reactjs e React-Native e estilizado com Styled-Component. Projeto feito para treinamento pessoal apenas para visualizar minhas habilidade no Front - End.",
         DoceBrigadeiroGithub: "https://github.com/FahCosta12/doce-brigadeiro-mobile.git",
+        DoceBrigadeiroWebSite: "",
 
     };
     return(
