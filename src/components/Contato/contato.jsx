2f6f22e9c59d4d28d1c2dc839d1d431d01e65892
Fabrício Costa <fabricio_matheus@outlook.com>
2024-08-20 22:49:26 -0300

    conserto dos campos de entrada do componente CONTATO

--- src/components/Contato/contato.jsx
+++ src/components/Contato/contato.jsx
@@ -12,16 +12,16 @@ const Contato = () => {
     const [enviado, setEnviado] = useState(false);
 
     const handleChange = (e) => {
-        const { nome, value } = e.target;
-        setFormData({
-            ...formData,
-            [nome]: value,
-        });
+        const { name, value } = e.target;
+        setFormData(prevFormData =>({
+            ...prevFormData,
+            [name]: value,
+        }));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Formulario enviado!");
+        console.log("Mensagem enviada!");
         setEnviado(true);
     };
 
@@ -29,12 +29,12 @@ const Contato = () => {
         <section className="container-contato">
             <h2>Contate-me</h2>
             {enviado ? (
-                <p className="mensagem-enviada">O Formulario foi enviado!</p>
+                <p className="mensagem-enviada">A sua mensagem foi enviado!</p>
             ) : (
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="nome">Nome:</label>
-                        <input type="text" id="nome" value={formData.nome} onChange={handleChange} required />
+                        <input type="text" id="nome" name="nome" value={formData.nome} onChange={handleChange} required />
                     </div>
 
                     <div className="form-group">
@@ -44,11 +44,12 @@ const Contato = () => {
 
                     <div className="form-group">
                         <label htmlFor="mensagem">Mensagem:</label>
-                        <textarea name="mensagem" id="mensagem" value={formData.mensagem} onChange={handleChange} required />
+                        <textarea type="text" name="mensagem" id="mensagem" value={formData.mensagem} onChange={handleChange} required />
                     </div>
 
                     <a type="submit" href="mailTo:fabricio_matheus@outlook.com">
                         <button className="btn-enviar">
+                            {" "}
                             {" "}
                             Enviar
                             <FaCheck> </FaCheck>{" "}
