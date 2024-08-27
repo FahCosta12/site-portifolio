696b01148637ebb8f5715c5762c8652e038f905e
Fabrício Costa <fabricio_matheus@outlook.com>
2024-08-27 00:07:07 -0300

    icone da tab, tecnicas de SEO e ajustes no componente PROJETOS aplicados

--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -9,10 +9,10 @@ const Projetos = () => {
             </h1>
 
             <div className="projeto">
-                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto01"/>
-                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto02"/>
-                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto03"/>
-                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto04"/>
+                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto-01"/>
+                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto-02"/>
+                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto-03"/>
+                <ProjetoBox projetoFoto={MolduraNotebook} projetoNome="Projeto-04"/>
             </div>
         </section>
     );
