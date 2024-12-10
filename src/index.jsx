d7ea3d40f0d7574e08b7aa728970f7ffb7df2b29
Fabrício Costa <fabricio_matheus@outlook.com>
2024-12-09 21:57:28 -0300

    feat: add integration with google analyticts

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,28 @@ import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 import "./index.css";
 
+const trackingId = 'G-0XXSEWXWRJ';
+
+const AppWithAnalytics = () => {
+	React.useEffect(() => {
+		const script = document.createElement('script');
+		script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+		document.body.appendChild(script);
+
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			window.dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', trackingId);
+	}, []);
+
+	return <App />;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<BrowserRouter>
-		<App />
+		<AppWithAnalytics />
 	</BrowserRouter>
 );
